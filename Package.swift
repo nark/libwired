36c50aa4b510828645e6eaef1e8b5173a4ec55fb
Luigi <45099401+ProfDrLuigi@users.noreply.github.com>
2024-02-29 14:30:18 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,20 +4,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "libwired2",
+    name: "libwired-osx2",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "libwired2",
-            targets: ["libwired2"]),
+            name: "libwired-osx2",
+            targets: ["libwired-osx2"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "libwired2"),
-        .testTarget(
-            name: "libwiredTests2",
-            dependencies: ["libwired2"]),
+            name: "libwired-osx2"),
     ]
 )
