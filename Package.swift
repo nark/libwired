2a26d0aa5fa877d1408a1e50e8384c5aea39335a
Luigi <45099401+ProfDrLuigi@users.noreply.github.com>
2024-02-29 14:41:10 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "libwired",
-            targets: ["WiredFrameworks"]),
+            targets: ["libwired-osx"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
