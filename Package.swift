345069d65db18b1b138bf0060ded0aaaf14884d3
Luigi <45099401+ProfDrLuigi@users.noreply.github.com>
2024-02-29 14:38:11 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "libwired",
+            name: "libwired-osx",
             targets: ["WiredFrameworks"]),
     ],
     targets: [
