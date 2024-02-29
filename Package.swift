4fdb3af412b00d114c0a2e2096649ab863bb9064
Luigi <45099401+ProfDrLuigi@users.noreply.github.com>
2024-02-29 14:40:06 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "libwired",
+    name: "libwired-osx",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
