e160d816c5984071c7261a64985701b134540929
Luigi <45099401+ProfDrLuigi@users.noreply.github.com>
2024-02-29 14:58:30 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,26 +1,18 @@
 // swift-tools-version:5.3
 import PackageDescription
-
 let package = Package(
-    name: "WiredClient",
-    platforms: [
-        .macOS(.v10_15), // Set appropriate platform versions
-        .iOS(.v13),      // if your project targets specific OS versions.
-    ],
+    name: "libwired",
     products: [
-        .library(
-            name: "WiredClient",
-            targets: ["WiredClient"]
-        ),
+        .library(name: "libwired", targets: ["libwired"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ProfDrLuigi/libwired.git", .branch("master")),
     ],
     targets: [
-        .target(
-            name: "WiredClient",
-            dependencies: ["libwired"]
-        ),
-        // Add other targets if needed
+        .systemLibrary(
+            name: "libwired",
+            ]
+        )
     ]
 )
+
+
